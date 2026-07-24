aab0100bae3c579c59ebdbcf7ac590ed85d8640a
t2405 <tusharbanga3030@gmail.com>
2026-07-24 09:36:20 +0530

    56

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import './lib/fetch-interceptor';
-import App from './Appx';
+import App from './App';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
